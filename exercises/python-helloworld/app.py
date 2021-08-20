2db97d3155a4ac1be28f5f88d2e5ebf0c2268fb8
Yogeshkumar1999 <malepatiyogeshkumar@gmail.com>
2021-08-20 17:57:06 +0530

    Added all the changes
    added all the changes

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, json
+import datetime
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/status")
+def health_check():
+
+    response = app.response_class(
+                response = json.dumps({"result": "OK - its healthy"}),
+                status = 200,
+                mimetype = 'application/json'
+            )
+    app.logger.info('status request success')
+    return response 
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+                response = json.dumps({'data': {'UserCount': 140,
+                    'UserCountActive': 23}}),
+                status = 200,
+                mimetype = 'application/json'
+            )
+    app.logger.info('metrics request success')
+    return response 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    app.run(host='0.0.0.0', port=8080)
--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,10 +1,13 @@
 import sqlite3
+from datetime import datetime
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
+import logging
 from werkzeug.exceptions import abort
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
+TIMEFORMAT = "%d/%m/%Y, %H:%m:%S"
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
@@ -36,13 +39,17 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.error("%s, A non-existing article is accessed artical id %d and a 404 page is returned."%(str(datetime.now().strftime(TIMEFORMAT)), post_id))
       return render_template('404.html'), 404
     else:
+      app.logger.info("%s, Article \"%s\" retrieved!"%(str(datetime.now().strftime(TIMEFORMAT)), post['title']))
       return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info( "%s, \"About Us\" page is retrieved."%str(str(datetime.now().strftime(TIMEFORMAT))))
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -53,8 +60,10 @@ def create():
         content = request.form['content']
 
         if not title:
+            app.logger.error("%s, Title requiered in order to post an article"%str(datetime.now().strftime(TIMEFORMAT)))
             flash('Title is required!')
         else:
+            app.logger.info("%s, A new article is created \"%s\" "%(str(datetime.now().strftime(TIMEFORMAT)), title))
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
@@ -65,6 +74,40 @@ def create():
 
     return render_template('create.html')
 
+
+@app.route('/healthz')
+def health():
+    response = app.response_class(
+            response = json.dumps({"result": "OK - its healthy"}),
+            status = 200,
+             mimetype = 'application/json'
+             )
+    return response
+
+@app.route('/metrics')
+def metrics():
+    logging.info("metrics request success")
+    connection = get_db_connection()
+    post_count = connection.execute('SELECT count(1) as count FROM posts').fetchone()
+    connection.close()
+    response = app.response_class(
+            response = json.dumps({"db_connection_count": 4, "post_count":
+                post_count['count']}),
+            status = 200,
+            mimetype = 'application/json'
+            )
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+   logging.root.handlers = []
+   logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO, filename='app.log')
+
+   # set up logging to console
+   console = logging.StreamHandler()
+   console.setLevel(logging.DEBUG)
+   # set a format which is simpler for console use
+   formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
+   console.setFormatter(formatter)
+   logging.getLogger("").addHandler(console)
+   app.run(host='0.0.0.0', port='3111', debug = True)
